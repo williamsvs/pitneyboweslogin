18de37cb4560617b36b4b9063690ce040a8f427f
williamsvs <williampooran@outlook.com>
2025-07-01 11:59:27 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,14 @@
+project = 'pitneyboweslogin'
+author = 'Your Name'
+release = '1.0'
 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'livechat.js',
+]
