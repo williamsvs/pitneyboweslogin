6e4d746bf8413649a8964bb3275340bb6afe567c
williamsvs <williampooran@outlook.com>
2025-07-01 11:58:08 +0530

    Update livechat.js

--- docs/_static/livechat.js
+++ docs/_static/livechat.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    var ze = document.createElement('script');
+    ze.id = 'ze-snippet';
+    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
+    document.body.appendChild(ze);
+});
